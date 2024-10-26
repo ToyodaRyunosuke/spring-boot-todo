78d2127940689bf0d7f76aa5d6018fd7f33c2140
ryunosuke0214 <ryunosuke0214@au.com>
2024-10-26 10:42:07 +0900

    fix: インポート漏れの対応

--- spring/src/main/java/com/example/todo/entity/Project.java
+++ spring/src/main/java/com/example/todo/entity/Project.java
@@ -3,6 +3,8 @@ package com.example.todo.entity;
 import java.time.LocalDateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -37,11 +39,11 @@ public class Project {
   @Column(name = "updated_at", nullable = false)
   private LocalDateTime updatedAt;
 
-  @PrePersis
+  @PrePersist
   private void onCreate() {
     LocalDateTime now = LocalDateTime.now();
     this.setCreatedAt(now);
-    this.setUpdateAt(now);
+    this.setUpdatedAt(now);
   }
 
   @PreUpdate
