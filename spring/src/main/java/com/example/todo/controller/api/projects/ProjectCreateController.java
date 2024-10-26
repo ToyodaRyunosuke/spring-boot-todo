d51c91b54f5db53542d7b44b61d53c4c4191503e
ryunosuke0214 <ryunosuke0214@au.com>
2024-10-26 10:43:14 +0900

    update: Controller の細かい誤字脱字の修正

--- spring/src/main/java/com/example/todo/controller/api/projects/ProjectCreateController.java
+++ spring/src/main/java/com/example/todo/controller/api/projects/ProjectCreateController.java
@@ -5,17 +5,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.todo.dto.request.projects.ProjectCreateRequest;
 import com.example.todo.entity.Project;
+import com.example.todo.service.projects.ProjectCreateService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/api/prijects")
+@RequestMapping("/api/projects")
 public class ProjectCreateController {
 
+  private final ProjectCreateService projectCreateService;
+
+  public ProjectCreateController(ProjectCreateService projectCreateService) {
+    this.projectCreateService = projectCreateService;
+  }
+
   @PostMapping
-  public ResponseEntity<Project> invoke(@Valid @RequestBody ProjectCreateRequest request){
-    //TODO:Projectを左l区政するロジックを実行予定。
-    Project project = new Project();
+  public ResponseEntity<Project> invoke(@Valid @RequestBody ProjectCreateRequest request) {
+    Project project = this.projectCreateService.invoke(request);
 
     return ResponseEntity.ok(project);
   }
