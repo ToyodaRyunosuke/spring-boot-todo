ede3e98eea3320cb53664f697eba8d4098a25494
ryunosuke0214 <ryunosuke0214@au.com>
2025-01-17 10:05:30 +0900

    補講１、２

--- spring/src/main/java/com/example/todo/dto/request/projects/ProjectCreateRequest.java
+++ spring/src/main/java/com/example/todo/dto/request/projects/ProjectCreateRequest.java
@@ -1,21 +1,23 @@
 package com.example.todo.dto.request.projects;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import jakarta.validation.constraints.NotEmpty;
 
 /**
- * @Getter  Lombookのアノテーション。クラス内の全てのフィールドに対して、getterメソッドを自動生成する
+ * @Getter Lombokのアノテーション。クラス内の全てのフィールドに対して、getterメソッドを自動生成する。
+ *
  * @RequiredArgsConstructor Lombokのアノテーション。フィールドを引数に持つコンストラクタを自動生成する。
  */
 @Getter
 @RequiredArgsConstructor
 public class ProjectCreateRequest {
 
-
-@NotEmpty
+  /**
+   * @NotEmpty 引数がnullまたは空文字の場合に、エラーメッセージを返すバリデーションアノテーション。未入力でリクエストされたくない文字列型のフィールドに対して付与する。
+   */
+  @NotEmpty
   private final String name;
 
   private final String summary;
-
 }
